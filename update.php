1958b529f958fae4d18552402e2c44debecdec6e
Ben Webb <bjwebb@debian.(none)>
2008-02-28 18:20:31 +0000

    OOps

---
+++ update.php
@@ -0,0 +1,15 @@
+<?php
+    include "config.php";
+    $con = mysql_connect($db_host,$db_name,$db_pass);
+    if (!$con) {
+      die('Could not connect: ' . mysql_error());
+    }
+    if ($db_prefix == "aa_") {
+        $name = "Audy Amnesty";
+    }
+    else {
+        $name = "Audenshaw Fair Trade Committeeization";
+    }
+    if (mysql_select_db($db_db, $con)); else die(mysql_error());
+    mysql_query("INSERT INTO `".$db_prefix."other` (type,text) VALUES ('title','$name')", $con) or die(mysql_error());
+?>
