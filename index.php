e491018d50977204e3d47c2c73fb7bc96cb78462
Ben Webb <bjwebb@webb-desktop.(none)>
2008-04-03 22:58:38 +0100

    Order members by level

--- index.php
+++ index.php
@@ -271,11 +271,14 @@ for ($i=0; $i<count($members); $i++) {
 }*/
 
 $userid = $_GET['userid'];
-$result = mysql_query("SELECT * FROM ".$db_prefix_forum."users WHERE state!='non'");
-while ($row = mysql_fetch_array($result)) {
-    if ($row['firstName'] != '') echo "<li><img src=\"http://two.xthost.info/bjwebb/adfa/".$row['state']."_32.png\" alt=\"".$row['state']."\"/><a href=\"?page=forum&amp;type=user&amp;userid=" . $row['userID'] . "\">" . $row['firstName']." ".$row['lastName'] . "</a></li>";
-    if ($members[$i][1])
-        echo "<span class=\"job\"> - ".$row['position']."</span>";
+$levels = array('gnu', 'peng', 'gull', 'fox', 'hat');
+for ($i=0; $i<sizeof($levels); $i++) {
+    $result = mysql_query("SELECT * FROM ".$db_prefix_forum."users WHERE state='".$levels[$i]."'");
+    while ($row = mysql_fetch_array($result)) {
+        if ($row['firstName'] != '') echo "<li><img src=\"http://two.xthost.info/bjwebb/adfa/".$row['state']."_32.png\" alt=\"".$row['state']."\"/><a href=\"?page=forum&amp;type=user&amp;userid=" . $row['userID'] . "\">" . $row['firstName']." ".$row['lastName'] . "</a></li>";
+        if ($members[$i][1])
+            echo "<span class=\"job\"> - ".$row['position']."</span>";
+    }
 }
 ?>
                         </ul>
